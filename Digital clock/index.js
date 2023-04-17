82eff9f22a9a213e56967f61ff33ad41e7f73270
Pradeep743 <118366690+Pradeep743@users.noreply.github.com>
2023-04-17 06:57:54 +0530

    Update index.js

--- Digital clock/index.js
+++ Digital clock/index.js
@@ -1,32 +1,31 @@
-const hourEl = document.getElementById("hour")
-const mintueEl = document.getElementById("minutes")
-const secondEl = document.getElementById("seconds")
+const hours = document.getElementById('hours')
+const minutes = document.getElementById('minutes')
+const seconds = document.getElementById('seconds')
+let ampm = document.getElementById('ampm')
 
-const ampmEl = document.getElementById("ampm")
 
 function updateClock() {
     let h = new Date().getHours();
     let m = new Date().getMinutes();
     let s = new Date().getSeconds();
-    let ampm = "AM";
+    ampm = "AM"
 
-    if(h > 12) {
-        h = h - 12;
+    if( h > 12) {
+        h = h - 12
         ampm = "PM";
-    }
+    } 
+    
+    h = h < 10 ? "0" + h : h;
+    m = m < 10 ? "0" + m : m;
+    s = s < 10 ? "0" + s : s;
 
-    h = h < 10 ? "0" + h : h; 
-    m = m < 10 ? "0" + m : m; 
-    s = s < 10 ? "0" + s : s; 
+    hours.innerText = h
+    minutes.innerText = m
+    seconds.innerText = s
 
-    hourEl.innerText = h;
-    mintueEl.innerText = m;
-    secondEl.innerText = s;
-    ampmEl.innerText = ampm;
     setTimeout(() => {
-        updateClock();
-    }, 1000);
-
+        updateClock()
+    }, 1000)
 }
 
-updateClock();
+updateClock()
